9ac869a4dee5c91a7e919d70b647aa9f37b570b6
Aldiansa Kautsar <aldiansakautsarramadhani@gmail.com>
2025-08-27 17:03:04 +0700

    initial backend layout + contoh table & form

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/admin', 'Admin\AdminController::index');
+$routes->get('/admin/form', 'Admin\AdminController::form');
+$routes->get('/admin/table', 'Admin\AdminController::table');
